00cdd532d5f75cfa93bc0dbbd0ac7bbb921f836b
JozhX <mizniks@gmail.com>
2023-12-22 21:53:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ current_time = now.strftime("%H:%M:%S")
 
 @app.route('/')
 def hello_world():
-    my_string = '<h1 style="text-align:center">Hello, World!</h1>' + '<h2 style="text-align:center">Servera laiks' + current_time + '</h2>'
+    my_string = '<h1 style="text-align:center">Hello, World!</h1>' + '<h2 style="text-align:center">Servera laiks ' + current_time + '</h2>'
     return my_string
