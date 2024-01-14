0562f4ccdd3c4c230729df8f1697355f0d7f04e3
JozhX <mizniks@gmail.com>
2024-01-14 20:27:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for 
+from flask import Flask, render_template, request, redirect, url_for, sys
 from database_connect import db_connect, cursor
 app = Flask(__name__)
 
